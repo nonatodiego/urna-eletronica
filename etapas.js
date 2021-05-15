2f81e95682f2c0efc4b73a2113c01721fe22cb28
Diego Nonato <42761103+nonatodiego@users.noreply.github.com>
2021-05-15 15:44:36 -0300

    atualizando arquivos urna

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Bender Futurama',
+                partido: 'ABC',
+                vice:'',
+                fotos: [
+                    {url: 'candidato-38111.png', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Homer Simpson',
+                partido: 'DEFG',
+                vice:'',
+                fotos: [
+                    {url: 'candidato-77222.png',legenda: 'Vereador',small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Mr.Popo',
+                partido: 'ABC',
+                vice: 'Homem de ferro',
+                fotos: [
+                    {url: 'candidato-p-99.png', legenda: 'Prefeito', small: false},
+                    {url: 'candidato-vp-99.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Super Mario',
+                partido: 'QWERTY',
+                vice: 'Jake hora de aventura',
+                fotos: [
+                    {url: 'candidato-p-84.png', legenda: 'Prefeito', small: false},
+                    {url: 'candidato-vp-84.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
