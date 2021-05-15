9d117069d7357fd0d24a3ff77f5610f279abd870
Diego Nonato <42761103+nonatodiego@users.noreply.github.com>
2021-05-14 22:01:01 -0300

    controle de areas na tela

---
+++ urna-eletronica-JS/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'futurama-bender.png', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'homer-simpson.png',legenda: 'Vereador',small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'iron-man.png', legenda: 'Prefeito', small: false},
+                    {url: 'jake.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'Mrpopo.png', legenda: 'Prefeito', small: false},
+                    {url: 'sadan.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
